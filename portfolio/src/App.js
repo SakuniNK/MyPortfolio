b3fb83d28481d391bc9fb542a3192ac92eafe56f
SakuniKodithuwakku <kodithuwakkusakuni@gmail.com>
2024-10-17 10:39:29 +0530

    yesterday part

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import React from "react";
 import './App.css';
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
---
+++ portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../Styles/Navbar.css";
+import logo from "../Assets/images/Sakuni1.svg";
+const Navbar = () => {
+    return (
+        <>
+        <nav className="nav-wrapper">
+            <div className="nav-content">
+                <img src={logo} alt="Logo"/>
+                <ul>
+                    <li>
+                        <a className="menu-item">Home</a>
+                    </li>
+                    <li>
+                        <a className="menu-item">Skills</a>
+                    </li>
+                    <li>
+                        <a className="menu-item">Volunteer Experience</a>
+                    </li>
+                    <li>
+                        <a className="menu-item">Projects</a>
+                    </li>
+                    <li>
+                        <a className="menu-item">Contact Me</a>
+                    </li>
+
+                    <button className="contact-btn" onClick={() => {
+                    }}>
+                        Hire Me
+                    </button>
+                </ul>
+
+                <button className="menu-btn" onClick={() => {}}>
+                    <span
+                        class={"material-symbols-outlined"}
+                        style={ {fontSize:"1.8rem" } }
+                        >
+                         menu
+                    </span>
+                </button>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar;
