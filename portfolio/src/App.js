f90e4902a599b9ca768cfd973bfea3554df105c5
SakuniKodithuwakku <kodithuwakkusakuni@gmail.com>
2024-10-20 21:19:19 +0530

    my works

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import './App.css';
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Skills from './Components/Skills';
+
 function App() {
   return (
     <>
     <Navbar/>
      <div className="container">
        <Hero/>
+       <Skills/>
      </div>
 
     </>
